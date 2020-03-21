724ebfda377a885879af4eacbb87578bf74886de
Salem <salemmakansi17@gmail.com>
2020-03-21 13:11:44 -0400

    Update main.php

--- main.php
+++ main.php
@@ -1,12 +1,12 @@
 <?php 
 
     include("abrir_conexion.php");
-    $nombre = $_POST["nombre"];
-    $apellido = $_POST["apellido"];
-    $edad  = $_POST["edad"];
+    $nombre   =  $_POST["nombre"];
+    $apellido =  $_POST["apellido"];
+    $edad     =  $_POST["edad"];
 
-    $insertar= "INSERT INTO $tabla (nombre,apellido,edad) VALUES ('$nombre','$apellido','$edad')";
-    $resultado = mysqli_query($dbconnect,$insertar);
+    $insertar   = "INSERT INTO $tabla (nombre,apellido,edad) VALUES ('$nombre','$apellido','$edad')";
+    $resultado  = mysqli_query($dbconnect,$insertar);
     
     if(!$resultado){
         echo "error al reistrarse";
